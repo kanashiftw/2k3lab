3216a440f2e36ec198a2941e4e8585a7601d831a
kanashiftw <92251137+kanashiftw@users.noreply.github.com>
2022-09-13 20:22:52 +0300

    Update Lab3z1.cpp

--- Lab3z1.cpp
+++ Lab3z1.cpp
@@ -1,25 +1,23 @@
-﻿#include <iostream>
+#include <iostream>
 #include <string>
 #include <vector>
 #include <stdlib.h>
 #include <sstream>
-
 using namespace std;
-struct Logger {
-    void logToScreen(){
-    
-    }
-    void logToFile(const string& filename){
+#define login(name) cout << "\n -->" << name<<'\n';
+#define logout(name) cout << "\n <--" << name<<'\n';
 
-    }
-};
+
 class Systematize {
 private:
     char c[6];
     vector<int> v;
 public:
     Systematize(string some_text) {
+        login("Systematize");
+        cout << endl;
         int k = 0;
+        cout << "Операторы: ";
         for (int i = 0; i < some_text.length(); i++) {
             if (!isdigit(some_text[i])) {
                 c[k] = some_text[i];
@@ -30,10 +28,18 @@ public:
         }
         cout << endl;
         stringstream ss(some_text);
+        cout << "Операнды: ";
         copy(istream_iterator<int>(ss), {}, back_inserter(v));
         copy(begin(v), end(v), ostream_iterator<int>(cout, " "));
+        cout << endl;
+        logout("Systematize");
 }
+    void priorities() {
+
+    }
     void calculate() {
+        login("calculate");
+        cout << endl;
         int k = 1;
         int tmp = v[0];
         for (int i = 0; i < 3;i++) {
@@ -57,14 +63,20 @@ public:
             }
         }
         cout << "Результат: " << tmp;
+        cout << endl;
+        logout("calculate");
     }
 };
+
 int main()
 {
+    login("main");
+    cout << endl;
     setlocale(LC_ALL, "Russian");
     string line = "8-2/2+1";
-    Systematize a (line);
+    logout("main");
+    cout << endl;
+    Systematize a(line);
     a.calculate();
 }
-
 
