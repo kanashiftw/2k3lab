f97bbcfe9a35d2fd3578adb8be722b34da90993c
kanashiftw <92251137+kanashiftw@users.noreply.github.com>
2022-09-23 21:19:01 +0300

    Update Lab3z2.cpp

--- Lab3z2.cpp
+++ Lab3z2.cpp
@@ -211,14 +211,19 @@ int main()
 {
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
-    Shop shop_test( "Пятерочка" ,1998, 88005555505, 178000000000, TRUE);
+    Shop shop_test("Пятерочка", 1998, 88005555505, 178000000000, TRUE);
     shop_test.displayShop();
     shop_test.seriallize("file.txt");
-    Market market_test("Сокол",60,2300,TRUE);;
+    Market market_test("Сокол", 60, 2300, TRUE);;
     market_test.displayShop();
-	market_test.seriallize();
-	market_test
+    market_test.seriallize("file.txt");
     BarberShop barbershop_test("Шалость", 24, 3100);
     barbershop_test.displayShop();
+    //RTTI
+    BarberShop* a = new BarberShop;
+    IShopFunc* b = dynamic_cast<IShopFunc*>(a);
+    cout << "\n\nРезультат приведения dynamic_cast для BarberShop:" << endl;
+    if (b) cout << "\tПриведение корректно.\nБазовый класс: "<< typeid(b).name()<<"\nПолученный класс: "<< typeid(a).name();
+    else cout << "\tПриведение некорректно." << endl;
 }
 
