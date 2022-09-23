6ce7c8104ca0b5611627a4bf774fbebe45f9eddb
kanashiftw <92251137+kanashiftw@users.noreply.github.com>
2022-09-23 19:25:43 +0300

    Update Lab3z1.cpp

--- Lab3z1.cpp
+++ Lab3z1.cpp
@@ -13,13 +13,27 @@ public:
     virtual void logcout(string text) = 0;
     virtual void logtofile(string text, string filename) = 0;
 };
+class IShuffle{
+public:
+    virtual void shuffleOperands() = 0;
+	virtual void shufflebetween2(int i, int j) = 0;
+};
 //Класс для работы с выражением
-class Expression : public ILoggable{
+class Expression : public ILoggable , public IShuffle{
 public:
     char c[100];
     vector<double> v;
     Expression() {
         v[20];
+        for (int i = 0; i < 20; i++) {
+            v[i] = 0;
+        }
+    }
+    Expression (int n){
+        v[n];
+        for (int i = 0; i < n; i++) {
+            v[i] = 0;
+        }
     }
     Expression(string some_text) {
         int k = 0;
@@ -41,11 +55,11 @@ public:
     ~Expression() {
         cout << "Элемент класса был удален." << endl;
     }
-    void setOperand(int pos, double value) {
+    void setOperand(double value, size_t pos) {
         v[pos - 1] = value;
         cout << "Операнду на позиции " << pos << " было присвоено значение " << value << endl;
     }
-    void shuffleoperands() {
+    virtual void shuffleOperands() override{
         cout << "Новый порядок операндов: " << endl;
         random_device rd;
         mt19937 g(rd());
@@ -53,14 +67,14 @@ public:
         copy(v.begin(), v.end(), std::ostream_iterator<double>(std::cout, "   "));
         cout << endl;
     }
-    void shufflebetween2(int i, int j) {
+    virtual void shufflebetween2(int i, int j) override{
         v[i] += v[j];
         v[j] = v[i] - v[j];
         v[i] -= v[j];
         cout << "Операнду под индексом " << i << " было присвоено значение " << v[i] << endl;
         cout << "Операнду под индексом " << j << " было присвоено значение " << v[j] << endl;
     }
-    void setOperands(double * new_operands, int size) {
+    void setOperands(double * new_operands, size_t size) {
             cout << "Новый порядок операндов: " << endl;
         for (int i = 0; i < v.size(); i++) {
             if (i < size) {
@@ -131,9 +145,9 @@ int main()
     string line = "10-2*2+1*245-2.5+14-19/3";
     Expression a(line);
     a.setOperand(2, 4);
-    a.shuffleoperands();
+    a.shuffleOperands();
     a.shufflebetween2(1, 2);
-    double array[3] = { 1,2,3};
+    double array[3] = {1,2,3};
     a.setOperands(array, 3);
     a.calculate();
 }
