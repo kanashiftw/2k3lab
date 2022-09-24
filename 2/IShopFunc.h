a6435272680d2a19e40ce6f05ee475a8d3611fdb
kanashiftw <92251137+kanashiftw@users.noreply.github.com>
2022-09-24 17:04:25 +0300

    Delete 2 directory

--- 2/IShopFunc.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <string>;
-class IShopFunc {
-public:
-    virtual void setter() = 0;
-    virtual void displayShop() = 0;
-    virtual void seriallize(const std::string filename) = 0;
-    virtual void deseriallize(const std::string filename) = 0;
-};
