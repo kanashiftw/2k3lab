592869bdd8170d9934fed62529bf8b9f20b295fa
kanashiftw <92251137+kanashiftw@users.noreply.github.com>
2022-09-28 20:29:21 +0300

    Delete 1 directory

--- 1/IExpression.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-using namespace std;
-
-class IExpression
-{
-public:
-    virtual void calculate() = 0;
-    virtual void displayOperation() = 0;
-};
--- 1/IShuffle.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "IExpression.h"
-class IShuffle : public IExpression {
-public:
-    virtual void shuffleOperands() = 0;
-    virtual void shuffleOperands(int i, int j) = 0;
-};
